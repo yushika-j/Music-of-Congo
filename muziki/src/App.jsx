de998d9baf31617be8a0f4441db27f1fcdd95fdd
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-04-01 18:29:02 -0400

    modified:   muziki/src/App.jsx 	new file:   muziki/src/assets/ferre gola.jpg 	new file:   muziki/src/assets/franco.jpg 	new file:   muziki/src/assets/papawemba.jpg 	new file:   muziki/src/assets/samba.jpg 	new file:   muziki/src/assets/werrason.jpg

--- muziki/src/App.jsx
+++ muziki/src/App.jsx
@@ -1,41 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import FerreGola from './assets/ferre gola.jpg';
+import Franco from './assets/franco.jpg';
+import PapaWemba from './assets/papawemba.jpg';
+import Samba from './assets/samba.jpg';
+import Werrason from './assets/werrason.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    
     <div>
-
       <ul className="nav-bar">
         <li><a href="home.html">Home</a></li>
         <li><a href="explore.html">Explore</a></li> {/* will contain songs/albums */}
         <li><a href="login.html">Log In</a></li>
         <li><a href="about.html">About</a></li>
       </ul>
-    
+
       <div className='gallery'>
-        <a><img src="img.jpg" /></a>
+        <img src={FerreGola} alt="Ferre Gola" />
       </div>
 
       <div className='gallery'>
-        <a><img src="img.jpg" /></a>
+        <img src={Franco} alt="Franco" />
       </div>
 
       <div className='gallery'>
-        <a><img src="img.jpg" /></a>
+        <img src={PapaWemba} alt="Papa Wemba" />
       </div>
 
       <div className='gallery'>
-        <a><img src="img.jpg" /></a>
+        <img src={Samba} alt="Samba" />
       </div>
-      
-    
+
+      <div className='gallery'>
+        <img src={Werrason} alt="Werrason" />
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
