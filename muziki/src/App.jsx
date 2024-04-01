6e7ec42c6c73cfe36217c405ecc3697b8c7d1699
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-04-01 17:56:34 -0400

    modified:   muziki/src/App.css 	modified:   muziki/src/App.jsx

--- muziki/src/App.jsx
+++ muziki/src/App.jsx
@@ -10,10 +10,10 @@ function App() {
     <>
     <div>
       <ul className="nav-bar">
-        <li>Home</li>
-         <li>Explore</li> {/* will contain songs/albums */}
-        <li>Sign Up</li>
-        <li>About</li>
+        <li><a href="home.html">Home</a></li>
+        <li><a href="explore.html">Explore</a></li> {/* will contain songs/albums */}
+        <li><a href="login.html">Log In</a></li>
+        <li><a href="about.html">About</a></li>
       </ul>
     </div>
     <div>
