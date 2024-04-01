5dded6a59dd93556b65d57cd9caa4749d9ccf936
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-04-01 18:02:11 -0400

    created navigation bar

--- muziki/src/App.jsx
+++ muziki/src/App.jsx
@@ -7,20 +7,21 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    
     <div>
+
       <ul className="nav-bar">
         <li><a href="home.html">Home</a></li>
         <li><a href="explore.html">Explore</a></li> {/* will contain songs/albums */}
         <li><a href="login.html">Log In</a></li>
         <li><a href="about.html">About</a></li>
       </ul>
-    </div>
-    <div>
-      body
-    </div>
+    
+    
+      
+    
     <footer>footer</footer>
-    </>
+    </div>
   )
 }
 
