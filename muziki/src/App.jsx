3fd0dcaf8cb8f11e8c5d55db798ae96dbaf2c636
Emiliano1919 <emilianojg38@gmail.com>
2024-05-12 17:58:50 -0400

    Add image slider and Bold text

--- muziki/src/App.jsx
+++ muziki/src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import FerreGola from './assets/ferre gola.jpg';
 import Franco from './assets/franco.jpg';
 import PapaWemba from './assets/papawemba.jpg';
@@ -7,6 +10,16 @@ import Samba from './assets/samba.jpg';
 import Werrason from './assets/werrason.jpg';
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
   return (
     <div>
       <ul className="nav-bar julius-sans-one-regular">
@@ -19,32 +32,37 @@ function App() {
         </div>
       </ul>
 
-      <div className="marquee-container julius-sans-one-regular">
-        <h1 className="marquee-text">AFROBEAT — HIGHLIFE — JUJU — MAKOSSA — MBALAX — SOUKOUS — BENGA — TAARAB — KWAITO — FUJI — GENGE — ZOUK — COUPE DECALE — AFRO HOUSE — BONGO FLAVA — CHIMURENGA — AFRO POP — AFRICAN JAZZ — AFRO-SOUL — GOSPEL MUSIC — AFROBEAT — HIGHLIFE — JUJU — MAKOSSA — MBALAX — SOUKOUS — BENGA — TAARAB — KWAITO — FUJI — GENGE — ZOUK — COUPE DECALE — AFRO HOUSE — BONGO FLAVA — CHIMURENGA — AFRO POP — AFRICAN JAZZ — AFRO-SOUL — GOSPEL MUSIC — AFROBEAT — HIGHLIFE — JUJU — MAKOSSA — MBALAX — SOUKOUS — BENGA — TAARAB — KWAITO — FUJI — GENGE — ZOUK — COUPE DECALE — AFRO HOUSE — BONGO FLAVA — CHIMURENGA — AFRO POP — AFRICAN JAZZ — AFRO-SOUL — GOSPEL MUSIC — AFROBEAT — HIGHLIFE — JUJU — MAKOSSA — MBALAX — SOUKOUS — BENGA — TAARAB — KWAITO — FUJI — GENGE — ZOUK — COUPE DECALE — AFRO HOUSE — BONGO FLAVA — CHIMURENGA — AFRO POP — AFRICAN JAZZ — AFRO-SOUL — GOSPEL MUSIC
+      <div className="marquee-container">
+        <h1 className="marquee-text">AFROBEAT—HIGHLIFE—JUJU—MAKOSSA—MBALAX—SOUKOUS—BENGA—TAARAB—KWAITO—FUJI—GENGE—ZOUK—COUPE DECALE—AFRO HOUSE—BONGO FLAVA—CHIMURENGA—AFRO POP—AFRICAN JAZZ—AFRO-SOUL—GOSPEL MUSIC—AFROBEAT—HIGHLIFE—JUJU—MAKOSSA—MBALAX—SOUKOUS—BENGA—TAARAB—KWAITO—FUJI—GENGE—ZOUK—COUPE DECALE—AFRO HOUSE—BONGO FLAVA—CHIMURENGA—AFRO POP—AFRICAN JAZZ—AFRO-SOUL—GOSPEL MUSIC—AFROBEAT—HIGHLIFE—JUJU—MAKOSSA—MBALAX—SOUKOUS—BENGA—TAARAB—KWAITO—FUJI—GENGE—ZOUK—COUPE DECALE—AFRO HOUSE—BONGO FLAVA—CHIMURENGA—AFRO POP—AFRICAN JAZZ—AFRO-SOUL—GOSPEL MUSIC
         </h1>
       </div>
 
-      <div className='gallery'>
-        <img src={FerreGola} alt="Ferre Gola" />
-      </div>
-
-      <div className='gallery'>
-        <img src={Franco} alt="Franco" />
-      </div>
-
-      <div className='gallery'>
-        <img src={PapaWemba} alt="Papa Wemba" />
-      </div>
-
-      <div className='gallery'>
-        <img src={Samba} alt="Samba" />
-      </div>
-
-      <div className='gallery'>
-        <img src={Werrason} alt="Werrason" />
+      <div className="home-video-container julius-sans-one-regular">
+        <div className="left">
+          WE HAVE SOME GOOD MUSIKI
+        </div>
+        <div className="right">
+          <Slider {...settings}>
+            <div>
+              <img src={Samba} alt="Samba" />
+            </div>
+            <div>
+              <img src={FerreGola} alt="Ferre Gola" />
+            </div>
+            <div>
+              <img src={Franco} alt="Franco" />
+            </div>
+            <div>
+              <img src={PapaWemba} alt="Papa Wemba" />
+            </div>
+            <div>
+              <img src={Werrason} alt="Werrason" />
+            </div>
+          </Slider>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
