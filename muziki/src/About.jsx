ae8117fe0df98d5b78c8535c3a553f929800139d
Emiliano1919 <emilianojg38@gmail.com>
2024-07-17 14:09:40 -0400

    Add Styling to About

--- muziki/src/About.jsx
+++ muziki/src/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import './About.css';
+
 
 const About = () => {
   return (
-    <div>
-      <h1>About Musiki</h1>
-      <p>Musiki is dedicated to bringing you the best of Congo music.</p>
+    <div className='aboutContainer'>
+      <h1 className='bigTitle tac-one-regular'>Musiki</h1>
+      <div className='aboutText kode-mono-400'>
+        <p className='aboutT1'>Celebrating Congo's vibrant musical heritage and future, sharing diverse and authentic  music. Musiki is dedicated to bringing you the best of Congolese music, free from western norms</p>
+        <p className='aboutT2'>Brought to you with appreciation by two students.</p>
+      </div>
     </div>
   );
 };
